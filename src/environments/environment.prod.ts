14e2689cfede240e54d646aa12724e047a7e8ef9
alynevieira <alynealicevieira1@gmail.com>
2020-09-11 15:53:22 -0300

    Instalação e configuração do firebase hosting na aplicação, testado com http-server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
-  baseUrl: '',
-  firebaseConfig: {}
+  // baseUrl: '',
+  firebaseConfig: {
+    apiKey: "AIzaSyAyMGmxJ8tpObKj-GIP0bZaC4eQ0ju6im0",
+    authDomain: "eager2learn-d4bb0.firebaseapp.com",
+    databaseURL: "https://eager2learn-d4bb0.firebaseio.com",
+    projectId: "eager2learn-d4bb0",
+    storageBucket: "eager2learn-d4bb0.appspot.com",
+    messagingSenderId: "136159806002",
+    appId: "1:136159806002:web:ca570909b7da79ec39722b",
+    measurementId: "G-PDLDVR5DS9"
+  }
 };
